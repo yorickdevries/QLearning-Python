a87114df4d1c59b0e29a9e9489cb4d7340f1b7af
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-09-13 12:27:32 +0200

    set methods to lowercase

--- main/mysolution/MyEGreedy.py
+++ main/mysolution/MyEGreedy.py
@@ -3,16 +3,17 @@ from main.Agent import Agent
 from main.Maze import Maze
 from main.QLearning import QLearning
 
+
 class MyEGreedy:
 
-    def getRandomAction(self, agent, maze):
+    def get_random_action(self, agent, maze):
         # TODO to select an action at random in State s
         return None
 
-    def getBestAction(self, agent, maze, q_learning):
+    def get_best_action(self, agent, maze, q_learning):
         # TODO to select the best possible action currently known in State s.
         return None
 
-    def getEGreedyAction(self, agent, maze, q_learning, epsilon):
+    def get_egreedy_action(self, agent, maze, q_learning, epsilon):
         # TODO to select between random or best action selection based on epsilon.
         return None
