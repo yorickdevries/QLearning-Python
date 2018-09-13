a002079d869d00564e77acb7ed24143974268b1c
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-09-13 12:16:46 +0200

    Make MyEgreedy Class

--- main/mysolution/MyEGreedy.py
+++ main/mysolution/MyEGreedy.py
@@ -1,1 +1,13 @@
 class MyEGreedy:
+
+    def getRandomAction(self, agent, maze):
+        # TODO to select an action at random in State s
+        return None
+
+    def getBestAction(self, agent, maze, q_learning):
+        # TODO to select the best possible action currently known in State s.
+        return None
+
+    def getEGreedyAction(self, agent, maze, q_learning, epsilon):
+        # TODO to select between random or best action selection based on epsilon.
+        return None
