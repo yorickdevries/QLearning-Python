7732ba96edaae242a4a0896939767b603dc5948c
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-09-13 12:36:04 +0200

    add update_Q stub to myQLearning

--- main/mysolution/MyQLearning.py
+++ main/mysolution/MyQLearning.py
@@ -1,4 +1,10 @@
+from main.Action import Action
 from main.QLearning import QLearning
+from main.State import State
 
 
 class MyQLearning(QLearning):
+
+    def update_q(self, state, action, r, state_next, possible_actions, alfa, gamma):
+        # TODO Auto-generated method stub
+        return
