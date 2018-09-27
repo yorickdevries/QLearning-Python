6e7ba3bf6fa0729e45c5db1387ad6036ba0ddcd5
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-09-27 10:58:15 +0200

    Fix import path

--- main/mysolution/RunMe.py
+++ main/mysolution/RunMe.py
@@ -1,3 +1,7 @@
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+
 from main.Maze import Maze
 from main.Agent import Agent
 from main.mysolution.MyQLearning import MyQLearning
